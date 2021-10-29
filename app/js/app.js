4a7d22ae89a0ab39c3a79c47332ebd02dfa8f84b
Denis Belocerkovec <belocerkovecden@gmail.com>
2021-10-29 17:18:56 +0300

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+/*    let grass = document.getElementById('grass');
+    let person = document.querySelectorAll('.person');
+
+    console.log(grass.width + 'x' + grass.height);
+    console.log(grass.width + 'x' + grass.offsetHeight);
+    console.log(grass.width + 'x' + grass.clientHeight);
+    let bt = ((grass.clientHeight / 100) * 50) + 100
+    person.forEach(item => item.style.bottom = bt + 'px')*/
+
+});
